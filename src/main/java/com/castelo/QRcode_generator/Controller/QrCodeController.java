810da4e57744f7535ee02c4cf790c06bdcfbe329
FengCastelo <ianfcastelo7@gmail.com>
2025-05-22 12:35:35 -0300

    Code fixes

--- src/main/java/com/castelo/QRcode_generator/Controller/QrCodeController.java
+++ src/main/java/com/castelo/QRcode_generator/Controller/QrCodeController.java
@@ -2,9 +2,10 @@ package com.castelo.QRcode_generator.Controller;
 
 import com.castelo.QRcode_generator.DTOS.QrcodeGenerateRequest;
 import com.castelo.QRcode_generator.DTOS.QrcodeGenerateResponse;
+
 import com.castelo.QRcode_generator.Service.QrCodeGeneratorService;
-import com.google.zxing.WriterException;
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,19 +19,21 @@ import java.io.IOException;
 
     private QrCodeGeneratorService qrCodeGeneratorService;
 
-    public QrCodeController qrCodeController(QrCodeGeneratorService qrCodeService){
+    public QrCodeController qrCodeController(QrCodeGeneratorService qrCodeService) {
         this.qrCodeGeneratorService = qrCodeService;
     }
 
 
     @PostMapping
     public ResponseEntity<QrcodeGenerateResponse> generate(@RequestBody QrcodeGenerateRequest request) {
-    try {
-        QrcodeGenerateResponse response = this.qrCodeGeneratorService.generateQRCode(request.text());
-        return ResponseEntity.ok(response);
+        try {
+            QrcodeGenerateResponse response = this.qrCodeGeneratorService.generateQRCode(request.text());
+            return ResponseEntity.ok(response);
 
-    } catch (Exception error) {
-        return ResponseEntity.internalServerError().build();
+        } catch (Exception error) {
+            return ResponseEntity.internalServerError().build();
+
+        }
+
     }
-
 }
